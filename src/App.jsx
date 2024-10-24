f2c1d89825d6297a803984df7aca915f2d7ca55b
Sifat <sifath740@gmail.com>
2024-10-24 19:56:20 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './component/blogs/Blogs'
+import Header from './component/Header/header'
+
+function App() {
+
+  return (
+    <>
+     <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
